ebee5453b9c62ee1c6717e97993841462c981b89
Laurent LESAGE <laurent.lesage51@gmail.com>
2021-11-26 16:55:52 +0100

    21-11-26 16:54 add figure list with request ajax button in progress

--- src/Repository/FigureRepository.php
+++ src/Repository/FigureRepository.php
@@ -20,46 +20,14 @@ class FigureRepository extends ServiceEntityRepository
         parent::__construct($registry, Figure::class);
     }
 
-
-
     public function getFigureByLimit(int $page, int $limitPerPage)
     {
         $querybuilder = $this->createQueryBuilder('a')
-            ->setFirstResult(($page - 1)*$limitPerPage)
+            ->setFirstResult(($page - 1) * $limitPerPage)
             ->setMaxResults($limitPerPage)
             ->orderBy('a.updatedAt','DESC');
             
         return new Paginator($querybuilder);
     }
 
-
-
-    // /**
-    //  * @return Figure[] Returns an array of Figure objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Figure
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
