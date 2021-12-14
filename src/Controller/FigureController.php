3bb1a7d3e76d92e3e222d3e5e50726300c0b00a3
Laurent LESAGE <laurent.lesage51@gmail.com>
2021-12-14 18:01:56 +0100

    21-12-14 18:01 in progress

--- src/Controller/FigureController.php
+++ src/Controller/FigureController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\FigureRepository;
 use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Common\Collections\Expr\Value;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -41,13 +44,38 @@ class FigureController extends AbstractController
      * @return Response
      * 
      */
-    public function trickView($slug,FigureRepository $figureRepository, CommentRepository $commentRepository ){
+    public function trickView(
+        $slug,
+        FigureRepository $figureRepository,
+        CommentRepository $commentRepository,
+        Request $request, 
+        EntityManagerInterface $entityManager
+         ) {
+        $this->entityManager =  $entityManager;   
+
         //je récupère la figure qui correspond au slug
         $figure = $figureRepository->findOneBySlug($slug);
+
+        //Je récupère tous les commentaires lié à la figure
         $comments = $commentRepository->findBy(['figure' => $figure]);
 
-        return $this->render('core/figures/trick.html.twig', ['figure' => $figure, 'comments' => $comments]);
+        $newComment = new Comment();
+        //création du formulaire avec les propriétées de l'entitée Comment
+        $formComment = $this->createForm(CommentType::class, $newComment);
 
+        //renseigne l'instance $user des informations entrée dans le formulaire et envoyé dans la requête
+        $formComment->handleRequest($request);
+
+        if($formComment->isSubmitted() && $formComment->isValid()) {
+
+            //Persister le commentaire
+            $this->entityManager->persist($newComment);
+            $this->entityManager->flush();
+            //Redirection
+            return $this->redirectToRoute('homePage');
+        }
+
+        return $this->render('core/figures/trick.html.twig', ['figure' => $figure, 'comments' => $comments, 'formComment' => $formComment->createView()]);
     }
 
     /**
