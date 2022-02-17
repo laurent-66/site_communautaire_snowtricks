51a05b0dca0b339c15e64bd489d28ec45b685324
Laurent LESAGE <laurent.lesage51@gmail.com>
2022-02-17 14:02:16 +0100

    22-02-17 14:01

--- public/js/customCarousel.js
+++ public/js/customCarousel.js
@@ -1,8 +1,11 @@
 $('#carouselExampleControls').on('slide.bs.carousel', function (e) {
 
     var $e = $(e.relatedTarget);
+    //index élément courant
     var idx = $e.index();
+    //nombre d'item par slide 
     var itemsPerSlide = 6;
+    //récupère le nombre d'items total
     var totalItems = $('.carousel-item').length;
     
     if (idx >= totalItems-(itemsPerSlide-1)) {
