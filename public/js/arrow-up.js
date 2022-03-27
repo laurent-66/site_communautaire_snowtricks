43c4d40d874180661544c78c43a8f74c1669b46a
Laurent LESAGE <laurent.lesage51@gmail.com>
2022-03-27 11:06:02 +0200

    22-03-27 arrow-up in progress

---
+++ public/js/arrow-up.js
@@ -0,0 +1,6 @@
+const totalItemsTrick = document.querySelectorAll("container-card");
+if(totalItemsTrick.length > 12) {
+    arrowUp.style.display='block';	
+}else {
+    arrowUp.style.display='none';	
+}
