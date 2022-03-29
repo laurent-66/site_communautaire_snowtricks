bf9d508ec6d9d5404db81b0e45db05d677d4c25f
Laurent LESAGE <laurent.lesage51@gmail.com>
2022-03-29 12:10:47 +0200

    22-03-29 12:10 arrow-up with condition done

--- public/js/arrow-up.js
+++
@@ -1,6 +0,0 @@
-const totalItemsTrick = document.querySelectorAll("container-card");
-if(totalItemsTrick.length > 12) {
-    arrowUp.style.display='block';	
-}else {
-    arrowUp.style.display='none';	
-}
