9482fb8d44dee2e367c0921e5134833ddd0fc0ff
Laurent LESAGE <laurent.lesage51@gmail.com>
2022-05-09 14:58:41 +0200

    in progress

--- public/js/customCarousel.js
+++ public/js/customCarousel.js
@@ -17,4 +17,32 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
             }
         }
     }
-});
+}); 
+
+
+// const carouselExample = document.querySelector('#carouselExample');
+// const carouselItem = dosument.querySelector('.carousel-item');
+
+// carouselExample.addEventListener('slide.bs.carousel', (e) => {
+//     let e = e.relatedTarget;
+//     let idx = $e.index();
+//     let itemsPerSlide = 6;
+//     let totalItems = carouselItem.length;
+
+//     if(idx >= totalItems-(itemsPerSlide-1)) {
+//         let it = itemsPerSlide - (totalItems - idx);
+
+//         for (let i=0; i<it; i++) {
+//             // append slides to end
+//             if (e.direction=="left") {
+
+//                 carouselItem
+//                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
+//             }
+//             else {
+//                 carouselItem
+//                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
+//             }
+//         }
+//     }
+// })
