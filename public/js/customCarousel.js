0076b0cb0e8b49feb726bfbba75543a4915633b8
Laurent LESAGE <laurent.lesage51@gmail.com>
2022-05-10 10:24:46 +0200

    22-05-10 Carousel update done

--- public/js/customCarousel.js
+++ public/js/customCarousel.js
@@ -4,7 +4,7 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
     var idx = $e.index();
     var itemsPerSlide = 6;
     var totalItems = $('.carousel-item').length;
-    
+
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
