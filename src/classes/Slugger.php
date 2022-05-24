8a4cd59b3811175d0112a18e59689599a92497f4
Laurent LESAGE <laurent.lesage51@gmail.com>
2022-05-24 18:16:22 +0200

    22-05-24 in progress

--- src/classes/Slugger.php
+++ src/classes/Slugger.php
@@ -5,16 +5,9 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 class Slugger 
 {
 
-    // public function __construct(SluggerInterface $slugger)
-    // {
-    //     $this->slugger = $slugger;
-    // }
-
-
     public static function slugify(string $filename, SluggerInterface $sluggerInterface )
     {
 
-        // return $this->slugger->slug($filename);
         return $sluggerInterface->slug($filename);
 
     }
